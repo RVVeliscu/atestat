bce18ef0bb5df8f61bb2411413892561a6462f91
Veliscu Robert-Valentin <robert_velisculfv@yahoo.ro>
2019-05-22 09:39:18 +0300

    mai e putin

--- atestat/source.cpp
+++ atestat/source.cpp
@@ -2,19 +2,32 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <queue>
 #include <iostream>
+
+#define Max 99999999
 
 using namespace std;
 
 ifstream f("date.in");
+ifstream h("querry.in");
 ofstream g("date.out");
+
+struct drum {
+	int dist;
+	bool viz;
+};
 
 map <string, int> indici;
 map <string, int>::iterator it;
-vector <int> graf[1000];
 
-string cit, film, act;
-int numar_filme, k, id, nr_act, idf, ida;
+vector <int> graf[1000];
+vector <drum> viz;
+
+queue <int> c;
+
+string cit, film, act, linie, prim, ult;
+int numar_filme, k, id, nr_act, idf, ida, st, fin;
 
 int gaseste_in_graf(string);
 void leaga(int, int);
@@ -42,7 +55,26 @@ int main()
 		}
 
 	}
-	g << k;
+	
+		
+	//while(!h.eof())
+	//{
+		/*
+		getline(h, prim, '-');
+		getline(h, prim, '-');
+		getline(h, ult, '\n');
+
+		st = indici[prim];
+		fin = indici[ult];
+		bfs(st);
+
+		if (viz[fin].viz)
+			g << "Distanta intre actorii" << prim << " si " << ult << " este de " << viz[fin].dist / 2;
+		else
+			g << "Actorii " << prim << " si " << ult << " nu sunt conectati.";
+			*/
+	//}
+
 	return 0;
 }
 
@@ -53,19 +85,53 @@ int gaseste_in_graf(string nume)
 	if (it == indici.end())
 	{
 		indici.insert(it, pair<string, int>(nume, ++k));
-		//indici[nume] = ++k;
 		id = k;
 	}
 	else
 	{
 		id = it->second;
-		g << nume << '\n';
+		//g << nume << '\n';
 	}
 	return id;
 }
 
 void leaga(int ida, int idf)
 {
-	graf[ida].insert( graf[ida].end(), idf);
+	graf[ida].insert(graf[ida].end(), idf);
 	graf[idf].insert(graf[idf].end(), ida);
+}
+
+void bfs(int start)
+{
+	int nc;
+	viz[start].viz = true;
+	viz[start].dist = 0;
+
+	c.push(start);
+
+	while (!c.empty())
+	{
+		nc = c.front();
+		viz[nc].viz = true;
+		
+		for (int i = 0; i < graf[nc].size(); ++i)
+		{
+			if (viz[graf[nc][i]].viz == false)
+			{
+				viz[graf[nc][i]].viz = true;
+				viz[graf[nc][i]].dist = viz[nc].dist + 1;
+				c.push(graf[nc][i]);
+			}
+			else
+			{
+				if (viz[graf[nc][i]].dist > viz[nc].dist + 1)
+				{
+					viz[graf[nc][i]].dist = viz[nc].dist + 1;
+					c.push(graf[nc][i]);
+				}
+			}
+		}
+
+		c.pop();
+	}
 }
